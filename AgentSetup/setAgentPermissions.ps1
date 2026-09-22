d2077c75cc7abd635cf1bc2e2dd33bf658a11734
mrochon <sailingrock@live.com>
2026-09-22 14:33:18 -0700

    Now getting inherited roles

--- AgentSetup/setAgentPermissions.ps1
+++ AgentSetup/setAgentPermissions.ps1
@@ -1,2 +1,3 @@
+# NOTE: this will only do conenst, you must still define the permissions as requiredResourceAccess structure in the blueprint
 Connect-Entra -Scopes 'AgentIdentityBlueprint.UpdateAuthProperties.All'
 Add-EntraPermissionsToInheritToAgentIdentityBlueprintPrincipal -Scopes @("user.read","mail.read") -Roles @("https://graph.microsoft.com/mail.read","https://graph.microsoft.com/user.read.all")
